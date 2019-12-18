5644be319a8a66dc529ca84456ea656c168f6c31
Thomas Queiroz <thomasqueirozb@gmail.com>
2019-12-18 16:43:22 -0300

    Instalation script now from get.docker.com

--- first.sh
+++ first.sh
@@ -1,5 +1,23 @@
 #!/bin/sh
-sudo pacman -S docker --noconfirm --needed
+. /etc/os-release
+# if [ "$ID" = "ubuntu" ]; then
+    # sudo apt-get update
+    # sudo apt-get install apt-transport-https ca-certificates \
+    #     curl gnupg-agent software-properties-common
+    # curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+    # sudo add-apt-repository \
+    #    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+    #    $(lsb_release -cs) stable"
+
+    # sudo apt-get update
+    # sudo apt-get install docker-ce docker-ce-cli containerd.io
+if [ "$ID" = "arch" ]; then
+    sudo pacman -S docker --noconfirm --needed
+else
+    curl -sSL https://get.docker.com | sh
+fi
+
 sudo usermod -a -G docker $USER
 
 echo "Please log out or restart"
