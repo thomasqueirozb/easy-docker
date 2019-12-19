465d534ae2b535c63ca79dbeb9763c54831b006b
Thomas Queiroz <thomasqueirozb@gmail.com>
2019-12-19 13:17:45 -0300

    Moved Dockerfile to aux/files

--- aux/build.sh
+++ aux/build.sh
@@ -7,4 +7,4 @@ _GID=$(id -g)
 _GID_NAME=$(id -gn)
 BASE_IMAGE=$(echo $BASE_IMAGE | sed -e 's/[\/&]/\\&/g')
 
-sed "s/BASE_IMAGE/$BASE_IMAGE/g;s/_UID/$_UID/g;s/_GID_NAME/$_GID_NAME/g;s/_GID/$_GID/g;" Dockerfile | docker build -t $IMAGE_NAME -
+sed "s/BASE_IMAGE/$BASE_IMAGE/g;s/_UID/$_UID/g;s/_GID_NAME/$_GID_NAME/g;s/_GID/$_GID/g;" files/Dockerfile | docker build -t $IMAGE_NAME -
