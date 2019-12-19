581cf5bbbe61cf1e3f6d1b483522fd0cf6730624
Thomas Queiroz <thomasqueirozb@gmail.com>
2019-12-19 12:36:37 -0300

    BASE_IMAGE variable now has its charaters escaped when building

--- build.sh
+++ build.sh
@@ -5,5 +5,6 @@
 _UID=$(id -u)
 _GID=$(id -g)
 _GID_NAME=$(id -gn)
+BASE_IMAGE=$(echo $BASE_IMAGE | sed -e 's/[\/&]/\\&/g')
 
 sed "s/BASE_IMAGE/$BASE_IMAGE/g;s/_UID/$_UID/g;s/_GID_NAME/$_GID_NAME/g;s/_GID/$_GID/g;" Dockerfile | docker build -t $IMAGE_NAME -
